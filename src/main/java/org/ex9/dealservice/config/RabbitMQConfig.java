f294d021e7eda3374540238af6f414a484d1e16f
Артём Краковцев <blooender@gmail.com>
2025-08-17 22:00:29 +0700

    feat: add rabbit config

---
+++ src/main/java/org/ex9/dealservice/config/RabbitMQConfig.java
@@ -0,0 +1,119 @@
+package org.ex9.dealservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Конфигурация RabbitMQ.
+ * <p>
+ * Определяет все необходимые {@link org.springframework.amqp.core.Exchange},
+ * {@link org.springframework.amqp.core.Queue} и {@link org.springframework.amqp.core.Binding}
+ * для поддержки обработки сообщений о контрагентах,
+ * а также обработку «мертвых» сообщений и реализацию retry-политики.
+ * </p>
+ * @author Краковцев Артём
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String CONTRACTORS_EXCHANGE = "contractors_contractor_exchange";
+    public static final String DEALS_CONTRACTOR_QUEUE = "deals_contractor_queue";
+    public static final String DEALS_CONTRACTOR_ROUTING_KEY = "contractor.updated";
+
+    public static final String DEALS_DEAD_EXCHANGE = "deals_dead_exchange";
+    public static final String DEALS_CONTRACTOR_DEAD_QUEUE = "deals_contractor_dead_queue";
+    public static final String DEALS_DEAD_ROUTING_KEY = "dead.contractor";
+
+    public static final String DEALS_RETRY_EXCHANGE = "deal_contractor_dead_exchange";
+    public static final String DEALS_RETRY_ROUTING_KEY = "retry.contractor";
+
+    /**Конвертер для десериализации payload из сообщения*/
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    /**Контейнер ручным подтверждением сообщений (MANUAL ACK)*/
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter converter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(converter);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        return factory;
+    }
+
+    //contractor
+    /**Обменник, в который публикует сервис контрагентов*/
+    @Bean
+    public TopicExchange contractorsExchange() {
+        return new TopicExchange(CONTRACTORS_EXCHANGE, true, false);
+    }
+
+    /**Основная очередь сервиса сделок*/
+    @Bean
+    public Queue dealsQueue() {
+        return QueueBuilder.durable(DEALS_CONTRACTOR_QUEUE)
+                .withArgument("x-dead-letter-exchange", DEALS_DEAD_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", DEALS_DEAD_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(dealsQueue())
+                .to(contractorsExchange())
+                .with(DEALS_CONTRACTOR_ROUTING_KEY);
+    }
+
+    //dead
+    /**dead-letter обменник*/
+    @Bean
+    public TopicExchange dealsDeadExchange() {
+        return new TopicExchange(DEALS_DEAD_EXCHANGE, true, false);
+    }
+
+    /**dead-letter очередь*/
+    @Bean
+    public Queue dealsContractorDeadQueue() {
+        return QueueBuilder.durable(DEALS_CONTRACTOR_DEAD_QUEUE)
+                .withArgument("x-message-ttl", 300000)
+                .withArgument("x-dead-letter-exchange", DEALS_RETRY_EXCHANGE)
+                .withArgument("x-dead-letter-routing-key", DEALS_RETRY_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Binding deadBinding() {
+        return BindingBuilder.bind(dealsContractorDeadQueue())
+                .to(dealsDeadExchange())
+                .with(DEALS_DEAD_ROUTING_KEY);
+    }
+
+    //retry
+    /**Retry-обменник, из которого сообщение возвращается в рабочую очередь*/
+    @Bean
+    public TopicExchange dealsRetryExchange() {
+        return new TopicExchange(DEALS_RETRY_EXCHANGE, true, false);
+    }
+
+    @Bean
+    public Binding retryBinding() {
+        return BindingBuilder.bind(dealsQueue())
+                .to(dealsRetryExchange())
+                .with(DEALS_RETRY_ROUTING_KEY);
+    }
+
+}
